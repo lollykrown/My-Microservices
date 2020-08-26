519dc4fb099cd27c8fc36230d953070aa3331770
Kay <joe_kayu@yahoo.com>
2020-08-26 21:03:52 +0100

    .

--- logger/app.js
+++ logger/app.js
@@ -1,18 +1,55 @@
 const express = require('express')
 const morgan = require('morgan'); //logger
 const bodyParser = require('body-parser');
+const Logger = require('./logger')();
+
+
 require('dotenv').config()
 
 const app = express();
+
+const logger = new Logger('app');
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan('tiny'))
 
 
-const mailingRouter = require('./api/routes/mailingRoutes')();
+app,post('/test', (req, res)=>{
+  const body = req.body
 
-app.use('/v1', mailingRouter);
+  let error = {}
+
+  logger.setLogData(body)
+
+  logger.info('Request received at /test', req.body)
+
+  if (body.name == null || body.name == '') {
+    logger.error('Name fild is empty')
+    error['name'] = 'Name fild is empty'
+  }
+  if (body.age == null || body.age == '') {
+    logger.error('age fild is empty')
+    error['age'] = 'age fild is empty'
+  }
+  if (body.gender == null || body.gender == '') {
+    logger.error('gender fild is empty')
+    error['gender'] = 'gender fild is empty'
+  }
+
+  if (Object.keys(error).length != 0) {
+    logger.error('Return error response', {
+      'success': false
+    })
+    res.send('Error')
+  } else {
+    logger.info('Return success response', {
+      'success': true
+    })
+    res.send('No Error')
+  }
+
+})
 
 
 port = process.env.PORT
