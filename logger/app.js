c38cce609c4811067b3f7af079ebd04f215aab95
Kay <joe_kayu@yahoo.com>
2020-08-27 14:50:47 +0100

    .

--- logger/app.js
+++ logger/app.js
@@ -15,7 +15,9 @@ const myStream = {
   }
 }
 
-app.use(morgan('combined', { stream: myStream }));
+// app.use(morgan('combined', { stream: myStream }));
+app.use(morgan('{"remote_addr": ":remote-addr", "remote_user": ":remote-user", "date": ":date[clf]", "method": ":method", "url": ":url", "http_version": ":http-version", "status": ":status", "result_length": ":res[content-length]", "referrer": ":referrer", "user_agent": ":user-agent", "response_time": ":response-time"}', {stream: myStream}));
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
