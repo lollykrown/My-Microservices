202218064fb7bf8f11f2ad9f13fd9b4b25acb86c
Kay <joe_kayu@yahoo.com>
2020-08-27 14:47:55 +0100

    .

--- logger/app.js
+++ logger/app.js
@@ -79,6 +79,20 @@ const options = {
   fields: ['message']
 };
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // add this line to include winston logging
+  logger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send('error');
+});
+
 port = process.env.PORT
 app.listen(port, function () {
   // console.log(`Listening on port ${port}...`)
