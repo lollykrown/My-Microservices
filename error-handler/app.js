89b755fa0aba9a4530dd2a0c07a96d4c54ebc913
Kay <joe_kayu@yahoo.com>
2020-08-27 17:17:03 +0100

    .

--- error-handler/app.js
+++ error-handler/app.js
@@ -1,101 +1,55 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const Logger = require('./logger');
-const morgan = require('morgan');
-let errorHelper = require('./errorHelpers');
-require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+let errorHelper = require("./errorHelpers");
+require("dotenv").config();
 
 const app = express();
 
-const logger = new Logger('app_root');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-
-//using the logger and its configured transports, to save the logs created by Morgan
-const myStream = {
-  write: (text) => {
-      logger.info(`From Morgan: ${text}`)
-  }
-}
-
-// app.use(morgan('combined', { stream: myStream }));
-app.use(morgan('{"remote_addr": ":remote-addr", "remote_user": ":remote-user", "date": ":date[clf]", "method": ":method", "url": ":url", "http_version": ":http-version", "status": ":status", "result_length": ":res[content-length]", "referrer": ":referrer", "user_agent": ":user-agent", "response_time": ":response-time"}', {stream: myStream}));
-
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.all('*', (req, res, next) => {
-  const url = req.protocol + '://' + req.get('host') + req.originalUrl;
-  logger.info('Incoming request', { method: req.method, url})
-
-  logger.debug('Incoming request verbose', {
-    headers: req.headers.host,
-    query: req.query,
-    body: req.body
-  });
-  next();
-})
-
-app.post('/test', (req, res)=>{
-  const {name, age, gender} = req.body
-
-  let error = {}
-
-  logger.setLogData(req.body)
-
-  logger.info('Request received at /test', req.body)
+app.post("/test", (req, res) => {
+  const { name, age, gender } = req.body;
 
   if (!name) {
-    logger.error('Name field is empty', {msg: 'error from name input'})
-    error['name'] = 'Name field is empty'
+    return res.status(400).json({
+      status: 400,
+      statusText: "Name not provided",
+      message: "Name param must be provided",
+      error: {
+        code: "NOT_FOUND",
+        message: "Name param must be provided",
+      }
+    });
   }
   if (!age) {
-    logger.error('Age field is empty')
-    error['age'] = 'age field is empty'
+    return res.status(400).json({
+      status: 400,
+      statusText: "age not provided",
+      message: "age param must be provided",
+      error: {
+        code: "NOT_FOUND",
+        message: "age param must be provided",
+      }
+    });
   }
   if (!gender) {
-    logger.error('Gender field is empty')
-    error['gender'] = 'gender fild is empty'
+    return res.status(400).json({
+      status: 400,
+      statusText: "gender not provided",
+      message: "gender param must be provided",
+      error: {
+        code: "NOT_FOUND",
+        message: "gender param must be provided",
+      }
+    });
   }
 
-  if (Object.keys(error).length != 0) {
-    logger.error('Error response', {
-      status: false,
-      message: 'unsuccessful'
-    })
-    res.send('Error')
-  } else {
-    logger.info('Success response', {
-      status: true,
-      message: 'success'
-    })
-    res.send('No Error')
-  }
-
-})
-
-const options = {
-  from: new Date() - (24 * 60 * 60 * 1000),
-  until: new Date(),
-  limit: 10,
-  start: 0,
-  order: 'desc',
-  fields: ['message']
-};
-
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // add this line to include winston logging
-//   logger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.send('error');
-// });
+  res.send({
+    status: true,
+    message: "success",
+  });
+});
 
 // Configure exception logger
 app.use(errorHelper.logErrors);
@@ -104,8 +58,8 @@ app.use(errorHelper.clientErrorHandler);
 // Configure catch-all exception middleware last
 app.use(errorHelper.errorHandler);
 
-port = process.env.PORT
+port = process.env.PORT;
 app.listen(port, function () {
   // console.log(`Listening on port ${port}...`)
-  logger.info(`App launched on port ${port}...`)
-}) 
+  console.log(`App launched on port ${port}...`);
+});
