0689c76af4ea8a2b5838157b39cc4d4920b99d8d
Kay <joe_kayu@yahoo.com>
2020-09-07 09:12:14 +0100

    .

--- error-handler/app.js
+++ error-handler/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 let errorHelper = require("./errorHelpers");
-const nodemailer = require('nodemailer')
+const nodemailer = require("nodemailer");
+const accessories = require("./accessories.json");
 
 require("dotenv").config();
 
@@ -11,14 +12,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send('Home')
-})
+  res.send("Home");
+});
 
 app.post("/test", (req, res) => {
   const { name, age, gender } = req.body;
 
   //intentional error on next line to trigger an error
-  const yu = fetch('url')
+  const yu = fetch("url");
 
   if (!name) {
     return res.status(400).json({
@@ -28,7 +29,7 @@ app.post("/test", (req, res) => {
       error: {
         code: "NOT_FOUND",
         message: "Name param must be provided",
-      }
+      },
     });
   }
   if (!age) {
@@ -39,7 +40,7 @@ app.post("/test", (req, res) => {
       error: {
         code: "NOT_FOUND",
         message: "age param must be provided",
-      }
+      },
     });
   }
   if (!gender) {
@@ -50,7 +51,7 @@ app.post("/test", (req, res) => {
       error: {
         code: "NOT_FOUND",
         message: "gender param must be provided",
-      }
+      },
     });
   }
 
@@ -60,6 +61,26 @@ app.post("/test", (req, res) => {
   });
 });
 
+app.get("/read-file", (req, res) => {
+  (async function auth() {
+    try {
+      const newArr = accessories.map((item => {
+        const patt = '/w3schools/i';
+      }))
+      res.status(200).json({
+        status: true,
+        message: "success",
+      });
+    } catch (err) {
+      logger.error("Internal Server Error", err.stack);
+      debug(err.stack);
+      res.status(500).json({
+        message: "Internal Server Error",
+      });
+    }
+  })();
+});
+
 // Configure exception logger
 app.use(errorHelper.logErrors);
 // Configure client error handler
@@ -67,27 +88,32 @@ app.use(errorHelper.clientErrorHandler);
 // Configure catch-all exception middleware last
 app.use(errorHelper.errorHandler);
 
-const transport = nodemailer.createTransport('SMTP', { // [1]
+const transport = nodemailer.createTransport({
+  // [1]
   service: "Gmail",
   auth: {
     user: process.env.USER,
-    pass: process.env.PASSWORD
-  }
-})
+    pass: process.env.PASSWORD,
+  },
+});
 
-if (process.env.NODE_ENV === 'production') { // [2]
-  process.on('uncaughtException', function (er) {
-    console.error(er.stack) // [3]
-    transport.sendMail({
-      from: process.env.USER,
-      to: 'joe_kayu@yahoo.com',
-      subject: er.message,
-      text: er.stack // [4]
-    }, function (er) {
-       if (er) console.error(er)
-       process.exit(1) // [5]
-    })
-  })
+if (process.env.NODE_ENV === "production") {
+  // [2]
+  process.on("uncaughtException", function (er) {
+    console.error(er.stack); // [3]
+    transport.sendMail(
+      {
+        from: process.env.USER,
+        to: "joe_kayu@yahoo.com",
+        subject: er.message,
+        text: er.stack, // [4]
+      },
+      function (er) {
+        if (er) console.error(er);
+        process.exit(1); // [5]
+      }
+    );
+  });
 }
 
 port = process.env.PORT;
