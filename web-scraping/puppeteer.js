d604ab8a1849ae2fc78de2a022b5aa131b792bfc
Kay <joe_kayu@yahoo.com>
2020-09-18 11:09:07 +0100

    .

--- web-scraping/puppeteer.js
+++ web-scraping/puppeteer.js
@@ -3,9 +3,23 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.setViewport({width:1280, height:800})
+
+//   // Set your page viewport
+//   await page.setViewport({width:1280, height:800})
   await page.goto('https://carmalou.com');
-  await page.screenshot({path: 'example.png'});
+
+//   // Get page screenshot
+//   await page.screenshot({path: 'carmalou.png', fullPage:true});
  
+ //Get the "viewport" of the page, as reported by the page.
+  const dimensions = await page.evaluate(() => {
+    return {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+      deviceScaleFactor: window.devicePixelRatio
+    };
+  });
+ 
+  console.log('Dimensions:', dimensions);
   await browser.close();
 })();
