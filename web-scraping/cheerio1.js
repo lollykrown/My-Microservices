45887f7b7dc035dd4d2e4a0465522574f3c4def6
Kay <joe_kayu@yahoo.com>
2020-09-18 11:03:57 +0100

    puppeteer

---
+++ web-scraping/cheerio1.js
@@ -0,0 +1,25 @@
+const express = require('request')
+const bodyParser = require('cheerio');
+const request = require('request');
+
+request('http://codedemos.com/sampleblog', (error, response, html) =>{
+  if(!error && response.statusCode == 200){
+    console.log(html)
+    // const $ = cheerio.load(html);
+
+    const siteHeading = $('.site-heading');
+
+    // console.log(siteHeading.html())
+    // console.log(siteHeading.text)
+
+    // const output = siteHeading
+    // .children('h1')
+    // .parent()
+    // .text();
+
+    $('.nav-item a').each((i, el) => {
+      const item = $(el).text()
+      console.log(item)
+    }
+  }
+})
---
+++ web-scraping/puppeteer.js
@@ -0,0 +1,11 @@
+const puppeteer = require('puppeteer');
+ 
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({width:1280, height:800})
+  await page.goto('https://carmalou.com');
+  await page.screenshot({path: 'example.png'});
+ 
+  await browser.close();
+})();
