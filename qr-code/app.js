d02abd3c316bfb3b8e33396e3b79b9d759d8196e
Kay <joe_kayu@yahoo.com>
2020-09-21 11:48:05 +0100

    qr code

--- qr-code/app.js
+++ qr-code/app.js
@@ -2,7 +2,6 @@ const createError = require("http-errors");
 const express = require("express");
 const debug = require("debug")("app:root");
 const bobyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
 const QRCode = require("qrcode");
 
 // init app
@@ -16,31 +15,47 @@ app.use(bobyParser.urlencoded({ extended: false }));
 
 // loading the Get page
 app.get("/", (req, res) => {
-      res.render("qrcode", { data: '' });
+  res.render("qrcode", { data: "" });
 });
 
 // loading the post page
 app.post("/", (req, res) => {
-    const { name } = req.body
-    QRCode.toDataURL(name, { errorCorrectionLevel: "H" }, (err, url) => {
-        if (err) { debug(err)}
-      debug('url', url);
-      qrcode.toString(name, { errorCorrectionLevel: "H" }, (err, text) => {
-        if (err) { debug(err)}
-      debug('text', text);
-      })
-    res.render("qrcode", { data: url});
-    })
-    // QRCode.toFile('test.png', name, {
-    //     color: {
-    //       dark: '#00F',  // Blue dots
-    //       light: '#0000' // Transparent background
-    //     }
-    //   }, function (err) {
-    //     if (err) throw err
-    //     console.log('done')
-    //     res.send('success')
-    //   })
+  const { name } = req.body;
+  QRCode.toDataURL(name, { errorCorrectionLevel: "H" }, (err, url) => {
+    if (err) {
+      debug(err);
+    }
+    debug("url", url);
+    
+    QRCode.toFile(
+      "test.png",
+      name,
+      {
+        errorCorrectionLevel: "H",
+        color: {
+          dark: "#00F", // Blue dots
+          light: "#0000", // Transparent background
+        },
+      },
+      (err) => {
+        if (err) {
+          debug(err);
+        }
+      }
+    );
+    res.render("qrcode", { data: url });
+  });
+  //   QRCode.toString(name, { errorCorrectionLevel: "H" }, (err, text) => {
+  //     if (err) { debug(err)}
+  //   debug('text', text);
+  //   })
+  // QRCode.toFile('test.png', name, {
+
+  //   }, function (err) {
+  //     if (err) throw err
+  //     console.log('done')
+  //     res.send('success')
+  //   })
 });
 
 // catch 404 and forward to error handler
@@ -63,6 +78,4 @@ module.exports = app;
 
 //Assign port
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () =>
-  debug(`Server is Starting on http://localhost:${PORT}`)
-);
+app.listen(PORT, () => debug(`Server is Starting on http://localhost:${PORT}`));
