f2e1238aaabe0c2318f7192b7ed04a0c8015db3a
Kay <joe_kayu@yahoo.com>
2020-09-22 13:02:44 +0100

    .

--- chat/app.js
+++ chat/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
 const path = require('path');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app)
@@ -11,20 +12,24 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 //runs when client connects
 io.on('connection', socket => {
-  console.log('New connection');
 
   // this emits to a single client
   // Welcome new user
-  socket.emit('message', 'Welcome to Chat app')
+  socket.emit('message', formatMessage('kayode',' Welcome to Chat App'))
 
   //broadcast when a user connects, broadcast emits to everyone except the sender
   //io.emit is to everyone with no exception, socket.emit is to a single user
-  socket.broadcast.emit('message', 'A user just joined the chat')
+  socket.broadcast.emit('message', formatMessage('kayode','A user just joined the chat'))
 
 
   //Runs when client disconnects
   socket.on('disconnect', () => {
-    io.emit('message', 'A user just left the chat')
+    io.emit('message', formatMessage('kayode','A user just left the chat'))
+  })
+
+  //Listen for chat message from the frontend
+  socket.on('chatMessage', (msg) => {
+    io.emit('message' , formatMessage('kayode',msg))
   })
 })
 
--- chat/public/js/main.js
+++ chat/public/js/main.js
@@ -1,5 +1,5 @@
-// const chatForm = document.getElementById('chat-form');
-// const chatMessages = document.querySelector('.chat-messages');
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
 // const roomName = document.getElementById('room-name');
 // const userList = document.getElementById('users');
 
@@ -10,10 +10,6 @@
 
 const socket = io();
 
-socket.on('message', msg => {
-  console.log(msg)
-})
-
 // // Join chatroom
 // socket.emit('joinRoom', { username, room });
 
@@ -23,51 +19,52 @@ socket.on('message', msg => {
 //   outputUsers(users);
 // });
 
-// // Message from server
-// socket.on('message', message => {
-//   console.log(message);
-//   outputMessage(message);
 
-//   // Scroll down
-//   chatMessages.scrollTop = chatMessages.scrollHeight;
-// });
 
-// // Message submit
-// chatForm.addEventListener('submit', e => {
-//   e.preventDefault();
+// Message submit
+chatForm.addEventListener('submit', e => {
+  e.preventDefault();
 
-//   // Get message text
-//   let msg = e.target.elements.msg.value;
-  
-//   msg = msg.trim();
-  
-//   if (!msg){
-//     return false;
-//   }
+  // Get message text
+  let msg = e.target.elements.msg.value;
+  msg = msg.trim();  
+  if (!msg){
+    return false;
+  }
 
-//   // Emit message to server
-//   socket.emit('chatMessage', msg);
+  // Emit message to server
+  socket.emit('chatMessage', msg);
 
-//   // Clear input
-//   e.target.elements.msg.value = '';
-//   e.target.elements.msg.focus();
-// });
+  // Clear input
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+});
 
-// // Output message to DOM
-// function outputMessage(message) {
-//   const div = document.createElement('div');
-//   div.classList.add('message');
-//   const p = document.createElement('p');
-//   p.classList.add('meta');
-//   p.innerText = message.username;
-//   p.innerHTML += `<span>${message.time}</span>`;
-//   div.appendChild(p);
-//   const para = document.createElement('p');
-//   para.classList.add('text');
-//   para.innerText = message.text;
-//   div.appendChild(para);
-//   document.querySelector('.chat-messages').appendChild(div);
-// }
+
+// Message from server
+socket.on('message', message => {
+  outputMessage(message);
+
+  // Scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+
+// Output message to DOM
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  const p = document.createElement('p');
+  p.classList.add('meta');
+  p.innerText = message.username;
+  p.innerHTML += ` <span>${message.time}</span>`;
+  div.appendChild(p);
+  const para = document.createElement('p');
+  para.classList.add('text');
+  para.innerText = message.text;
+  div.appendChild(para);
+  document.querySelector('.chat-messages').appendChild(div);
+}
 
 // // Add room name to DOM
 // function outputRoomName(room) {
