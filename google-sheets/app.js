d179196fa1b0c47ac41deb5a568ecdf06e99196a
Kay <joe_kayu@yahoo.com>
2021-01-06 13:59:09 +0100

    .

--- google-sheets/app.js
+++ google-sheets/app.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 
 // const keyfile = path.join(__dirname, 'credentials.json');
 // const keys = JSON.parse(fs.readFileSync(keyfile));
-const scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+const scopes = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive'];
 
 const uris = JSON.parse(process.env.REDIRECT_URIS)
 
@@ -33,6 +33,7 @@ const app = express();
 // app.use(bodyParser.urlencoded({ extended: false }))
 
 
+
 app.get('/oauth2callback', (req, res) => {
   const code = req.query.code;
 
@@ -46,9 +47,10 @@ app.get('/oauth2callback', (req, res) => {
     client.credentials = tokens;
     res.send('Authentication successful! Please return to the console.');
     server.close();
-    listMajors(client);
+    appendValues(client);
   });
 });
+
 
 // error handler
 // app.use(function(err, req, res, next) {
@@ -67,6 +69,7 @@ const server = app.listen(port, () => {
     console.log(`Listening on port ${port}...`)
 
     console.log('Authorize this app by visiting this url:', this.authorizeUrl);
+
   // open the browser to the authorize url to start the workflow
   // opn(this.authorizeUrl, {wait: false});
 });
@@ -77,8 +80,46 @@ const server = app.listen(port, () => {
  * 
  * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
  */
-function listMajors(auth) {
+
+const create = (auth) => {
   const sheets = google.sheets('v4');
+
+  const resource = {
+    properties: {
+      title: 'Sample'
+    },
+    sheets: [
+      {
+        sheetId: 1,
+        title: 'Sheet1',
+        index: 0,
+        // tabColor: {
+        //   object (Color)
+        // },
+      }
+    ]
+  };
+
+  sheets.spreadsheets.create(
+    {
+      auth: auth,
+      resource,
+      fields: 'spreadsheetId',
+    },
+    (err, res) => {
+      if (err) {
+        console.error('The API returned an error.');
+        throw err;
+      }
+      console.log(`Spreadsheet ID: ${res.data.spreadsheetId}`);
+
+    }
+  );
+}
+
+const read = (auth) => {
+  const sheets = google.sheets('v4');
+  
   sheets.spreadsheets.values.get(
     {
       auth: auth,
@@ -97,12 +138,164 @@ function listMajors(auth) {
         console.log('Name, Home State, Major:');
 
         rows.slice(0,3).map(row => console.log(`${row[0]}, ${row[3]}, ${row[4]}`))
-        // for (const row of rows) {
-        //   // Print columns A and E, which correspond to indices 0 and 4.
-        //   console.log(`${row[0]}, ${row[3]}, ${row[4]}`);
-        // }
       }
     }
   );
 }
 
+const readMultiple = (auth) => {
+  const sheets = google.sheets('v4');
+  
+  sheets.spreadsheets.values.batchGet(
+    {
+      auth: auth,
+      spreadsheetId: '1xGTbNEE_h3yzmtRsIB_vEa4_qneW-LseUk65pOjdbWo',
+      ranges: ['Sheet1!A2:E4', 'Sheet1!A5:E11'],
+    },
+    (err, res) => {
+      if (err) {
+        console.error('The API returned an error.');
+        throw err;
+      }
+      const rows = res.data.valueRanges;
+      if (rows.length === 0) {
+        console.log('No data found.');
+      } else {
+        console.log(`First Range: ${rows[0].values}, Second Range: ${rows[1].values}`);
+
+     }
+    }
+  );
+}
+const update = (auth) => {
+  const sheets = google.sheets('v4');
+
+  let values = [
+    ["Item", "Cost", "Stocked", "Ship Date"],
+    ["Wheel", "$20.50", "4", "3/1/2016"],
+    ["Door", "$15", "2", "3/15/2016"],
+    ["Engine", "$100", "1", "3/20/2016"],
+    ["Totals", "=SUM(B2:B4)", "=SUM(C2:C4)", "=MAX(D2:D4)"]
+    // Additional rows ...
+  ];
+  const resource = {
+    values,
+  };
+  const option = { raw: 'RAW', userEntered: 'USER_ENTERED'}
+  sheets.spreadsheets.values.update(
+    {
+      auth: auth,
+      spreadsheetId: '18IWXGB_KKeC3-tV2oRJGTjNvvftRossIRyqGhfY4SCU',
+      range: 'Sheet1!A1:D5',
+      valueInputOption: option.userEntered,
+      resource,
+    },
+    (err, res) => {
+      if (err) {
+        console.error('The API returned an error.');
+        throw err;
+      }
+     
+      console.log(`${res.data.updatedCells} cells updated on cell range: ${res.data.updatedRange}.`);
+
+    }
+  );
+}
+
+const updateMultiple = (auth) => {
+  const sheets = google.sheets('v4');
+  
+  const data = [
+      {
+        range: "Sheet1!A1:D1",
+        values: [
+          ["Item", "Cost", "Stocked", "Ship Date"],
+        ]
+      },
+      {
+        range: "Sheet1!A2:D4",
+        values: [
+          ["Wheel", "$20.50", "4", "3/1/2016"],
+          ["Door", "$15", "2", "3/15/2016"],
+          ["Engine", "$100", "1", "3/20/2016"],
+        ]
+      },
+      {
+        range: "Sheet1!A6:D6",
+        values: [
+          ["Totals", "=SUM(B2:B4)", "=SUM(C2:C4)", "=MAX(D2:D4)"]
+        ]
+      },
+      {
+        range: "Sheet2!A1:D5",
+        values: [ ["Item", "Cost", "Stocked", "Ship Date"],
+        ["Wheel", "$20.50", "4", "3/1/2016"],
+        ["Door", "$15", "2", "3/15/2016"],
+        ["Engine", "$100", "1", "3/20/2016"],
+        ["Totals", "=SUM(B2:B4)", "=SUM(C2:C4)", "=MAX(D2:D4)"]]
+      },
+    ];
+    
+  const option = { raw: 'RAW', userEntered: 'USER_ENTERED'}
+
+  const resource = {
+    data,
+    valueInputOption: option.userEntered,
+  };
+  sheets.spreadsheets.values.batchUpdate(
+    {
+      auth: auth,
+      spreadsheetId: '18IWXGB_KKeC3-tV2oRJGTjNvvftRossIRyqGhfY4SCU',
+      resource,
+    },
+    (err, res) => {
+      if (err) {
+        console.error('The API returned an error.');
+        throw err;
+      }
+     
+      console.log(`${res.data.responses[0].updatedCells} cells updated on cell range: ${res.data.responses[0].updatedRange}.
+      \n\n
+      ${res.data.responses[1].updatedCells} cells updated on cell range: ${res.data.responses[1].updatedRange}.`);
+
+    }
+  );
+}
+
+const appendValues = (auth) => {
+  const sheets = google.sheets('v4');
+
+  let values = [
+    [ 'Lollykrown',	'Male',	'4. Senior',	'MI',	'Math',	'Debate'],
+    ['Robert',	'Male',	'1. Freshman',	'CA',	'English',	'Track & Field'],
+    ['Sean',	'Male',	'1. Freshman',	'NH',	'Physics',	'Track & Field'],
+    ['Stacy',	'Female',	'1. Freshman',	'NY',	'Math',	'Baseball'],
+    ['Thomas',	'Male',	'2. Sophomore',	'RI',	'Art',	'Lacrosse'],
+  ]
+  const resource = {
+    values,
+  };
+  const option = { raw: 'RAW', userEntered: 'USER_ENTERED'}
+  sheets.spreadsheets.values.append(
+    {
+      auth: auth,
+      spreadsheetId: '1xGTbNEE_h3yzmtRsIB_vEa4_qneW-LseUk65pOjdbWo',
+      range: 'Sheet1!A1:F',
+      valueInputOption: option.raw,
+      resource,
+    },
+    (err, res) => {
+      if (err) {
+        console.error('The API returned an error.');
+        throw err;
+      }
+      console.log(`${res.data.updates.updatedCells} cells updated on cell range: ${res.data.updates.updatedRange}.`);
+
+    }
+  );
+}
+
+//Visit https://developers.google.com/sheets/api/samples/data 
+// and https://developers.google.com/sheets/api/guides/create
+// https://codelabs.developers.google.com/codelabs/sheets-api#0
+// for other excell sheet manipulation codes
